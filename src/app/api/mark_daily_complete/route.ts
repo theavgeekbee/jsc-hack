6d5df58dfcba909bb14f588f2aa2e731d0678cc3
unknown <nathan-lee1111@outlook.com>
2024-11-16 16:37:15 -0600

    /api/mark_daily_compelte

---
+++ src/app/api/mark_daily_complete/route.ts
@@ -0,0 +1,36 @@
+import {NextRequest, NextResponse} from "next/server";
+import {createClient} from "@/utils/supabase/server";
+
+export async function GET(req: NextRequest): Promise<NextResponse> {
+    const supabase = await createClient();
+    const {data: {user}, error: authError} = await supabase.auth.getUser();
+
+    if (authError || !user) {
+        return new NextResponse("User is not authenticated", {status: 401});
+    }
+
+    const daily_challenge = await supabase
+        .from("challenges")
+        .select("*")
+        .eq("day", new Date().toISOString().slice(0, 10))
+        .single()
+
+    const id = daily_challenge.data.id;
+
+    const row = await supabase
+        .from("users")
+        .select("*")
+        .eq("email", user.email)
+        .single();
+
+    if (row.data.completed_challenges.includes(id)) {
+        return NextResponse.json({ message: "Daily challenge already is completed" }, { status: 420 });
+    }
+
+    await supabase
+        .from("users")
+        .update({completed_challenges: [...row.data.completed_challenges, id]})
+        .eq("email", user.email);
+
+    return NextResponse.json({ message: "Marked daily challenge as complete" }, { status: 200 });
+}
