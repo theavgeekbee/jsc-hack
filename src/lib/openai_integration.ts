df4a24bf134368c56ae26bd7759f464deeccf111
unknown <nathan-lee1111@outlook.com>
2024-11-16 11:41:26 -0600

    modify system prompt and add roadmap

--- src/lib/openai_integration.ts
+++ src/lib/openai_integration.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 const openai: OpenAI = new OpenAI();
 
-const SYSTEM_PROMPT = "You're an AI planning assistant for an astronaut on a space mission. The astronaut wants new, innovative ideas on how they can combine their provided foods in a interesting and delicious way. The astronaut has provided a list of foods they have and want to use as well as special instructions, if any. Try to make the recipe follow a general theme. You do not have to use all of the ingredients. Feel free to be a bit unorthodox in your meal planning. Keep in mind that it may not be possible to deconstruct the original ingredients of the provided foods. Remember that they are on a space mission, so they can only combine foods and cook them.\n" +
+const SYSTEM_PROMPT = "You're an AI planning assistant for an astronaut on a space mission. The astronaut wants new, innovative ideas on how they can combine their provided foods in a interesting and delicious way. The astronaut has provided a list of foods they have and want to use as well as special instructions, if any. Try to make the recipe follow a general theme. You do not have to use all of the ingredients. Feel free to be a bit unorthodox in your meal planning. Keep in mind that it may not be possible to deconstruct the original ingredients of the provided foods. Remember that they are on a space mission, so consider those limitations.\n" +
     "\n" +
     "Ingredients:\n" +
     "${INGREDIENTS}\n" +
@@ -10,7 +10,9 @@ const SYSTEM_PROMPT = "You're an AI planning assistant for an astronaut on a spa
     "Special Instructions:\n" +
     "${SPECIAL_INSTRUCTIONS}";
 
-export async function generateRecipe(ingredients: string[], specialInstructions: string = "No Special Instructions"): Promise<string> {
+export async function generateRecipe(auth_token: string, ingredients: string[], specialInstructions: string = "No Special Instructions"): Promise<string> {
+    // todo: authenticate user with the auth_token to make sure they have access to the API
+
     const prompt = SYSTEM_PROMPT
         .replace("${INGREDIENTS}", ingredients.join("\n"))
         .replace("${SPECIAL_INSTRUCTIONS}", specialInstructions);
