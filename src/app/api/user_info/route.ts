7f6e121d04f9de06792fd20c5b773073285677d1
unknown <nathan-lee1111@outlook.com>
2024-11-16 15:56:01 -0600

    rename getUser

--- src/app/api/user_info/route.ts
+++ src/app/api/user_info/route.ts
@@ -1,7 +1,13 @@
+import { createClient } from "@/utils/supabase/server";
 import {NextRequest, NextResponse} from "next/server";
-import {generatePlan} from "@/lib/openai_integration";
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
-    return new NextResponse(null, { status: 200 });
-}
+    const supabase = await createClient();
 
+    const url = new URL(req.url);
+    const email = url.searchParams.get("email");
+
+    const user = await supabase.from("users").select("*").eq("email", email).single();
+
+    return NextResponse.json({data: user.data});
+}
