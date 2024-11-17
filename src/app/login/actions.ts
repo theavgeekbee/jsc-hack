72216c7694a42513dc7f6e8b22f5570d289d90f7
unknown <nathan-lee1111@outlook.com>
2024-11-16 19:53:16 -0600

    input validation

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -6,10 +6,15 @@ import {redirect} from 'next/navigation'
 import {createClient} from '@/utils/supabase/server'
 
 export async function login(formData: FormData) {
+    if (!formData.get('email') || !formData.get('password'))
+        return;
+
+    if ((formData.get('password') as string).length < 6)
+        return;
+
+
     const supabase = await createClient()
 
-    // type-casting here for convenience
-    // in practice, you should validate your inputs
     const data = {
         email: formData.get('email') as string,
         password: formData.get('password') as string,
