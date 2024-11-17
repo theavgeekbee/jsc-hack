2be9b11fcea75b17f4084a0d59bc959958713b3e
unknown <nathan-lee1111@outlook.com>
2024-11-17 04:02:56 -0600

    lint api routes

--- src/app/api/user_info/route.ts
+++ src/app/api/user_info/route.ts
@@ -1,7 +1,7 @@
 import {createClient} from "@/utils/supabase/server";
 import {NextRequest, NextResponse} from "next/server";
 
-export async function GET(req: NextRequest): Promise<NextResponse> {
+export async function GET(): Promise<NextResponse> {
     const supabase = await createClient();
 
     const { data: { user }} = await supabase.auth.getUser();
