e440658c54f1e00b95d00e0bc0871d694a1f18bb
unknown <nathan-lee1111@outlook.com>
2024-11-16 23:20:32 -0600

    /api/get_challenge

---
+++ src/app/api/get_challenge/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET(req: NextRequest): Promise<NextResponse> {
+    const supabase = await createClient();
+    const { data: { user }, error: authError } = await supabase.auth.getUser();
+
+    if (authError || !user) {
+        return new NextResponse("User is not authenticated", { status: 401 });
+    }
+
+    const key = req.nextUrl.searchParams.get("challenge_id");
+    if (!key) {
+        return new NextResponse("Missing challenge_id", { status: 400 });
+    }
+
+    const { data: challenge, error } = await supabase.from("challenges").select("*").eq("id", key).single();
+    if (error) {
+        return new NextResponse("Error fetching challenge", { status: 500 });
+    }
+    return new NextResponse(JSON.stringify(challenge), { status: 200 });
+}
