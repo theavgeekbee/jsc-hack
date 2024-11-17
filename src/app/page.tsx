2bea3f3e1c8c28296479e55eb9fe179da47a7aba
unknown <nathan-lee1111@outlook.com>
2024-11-16 19:19:08 -0600

    on home, redirect to login if not logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+'use server';
+
+import {createClient} from "@/utils/supabase/server";
+import {redirect} from "next/navigation";
 
 export default async function Home() {
+  const supabase = await createClient();
   const { data: { user } } = await supabase.auth.getUser();
 
-  // TODO: if user is not logged in, redirect to login page
+  if (!user) {
+    redirect("/login");
+  }
 
   return <>
     <h1>Welcome back</h1>
