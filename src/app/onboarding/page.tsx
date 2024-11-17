0713eddb255a92838f2ee6ae245e54f159379e9c
unknown <nathan-lee1111@outlook.com>
2024-11-16 20:10:39 -0600

    add onboarding

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+import {useState} from "react";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+
+export default function Onboarding() {
+    const [name, setName] = useState<undefined | string>(undefined);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    function submitForm(_: FormData) {
+        if (!name) {
+            return;
+        }
+
+        fetch("/api/update_name", {
+            method: "POST",
+            body: JSON.stringify({name}),
+        })
+            .then((response) => response.ok)
+            .then(res => res);
+    }
+
+    return (
+        <section>
+            <form>
+                <Input label={"Name"} id={"name"} type={"text"} onChange={
+                    (event) => setName(event.target.value)
+                }/>
+                <Button variant={"primary"} type={"submit"} formAction={submitForm}>Submit</Button>
+            </form>
+        </section>
+    );
+}
